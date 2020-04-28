091c910f332de9d1c97e26fdd0e4064975fdd60e
medvecky <alexey.medvecky@gmail.com>
2020-04-28 22:46:13 +0300

    add log fix

--- controller.js
+++ controller.js
@@ -17,12 +17,10 @@ const addUser = (req, res) => {
 };
 
 const getAllUsers = (req, res) => {
-    User.find({}, (err, docs) => res.json(docs)).select({count: 0, __v: 0, log:0});
+    User.find({}, (err, docs) => res.json(docs)).select({count: 0, __v: 0, log: 0});
 };
 
 const addExerciseToUserLog = (req, res) => {
-    console.log("Add exercise log");
-    console.log(req.body);
     const description = req.body.description;
     const duration = req.body.duration;
     const date = req.body.date;
@@ -35,17 +33,21 @@ const addExerciseToUserLog = (req, res) => {
             $push: {log: logEntry},
             $inc: {count: 1},
         }, (err, docs) => {
-            if(err) {
+            if (err) {
                 res.send('User does not exist.');
             } else {
-               let returnValue = docs;
-               console.log(docs);
-               returnValue.count += 1;
-               returnValue.log.push(logEntry);
-               res.json(docs);
+                let returnValue = {
+                    username: docs.username,
+                    _id: docs._id,
+                    description: description,
+                    duration: duration,
+                    date: logEntry.date
+                };
+                returnValue.count += 1;
+                returnValue.log.push(logEntry);
+                res.json(docs);
             }
         });
-
 };
 
 const getUserLog = (req, res) => {
@@ -55,34 +57,10 @@ const getUserLog = (req, res) => {
     const to = req.query['to'];
     const limit = req.query['limit'];
 
-    console.log(from);
-    console.log(to);
-    console.log(limit);
-
-    console.log(req.query);
-
-
-    const fromFilter = (currentDate, fromDate) => (fromDate
-        ? Date.parse(currentDate) > Date.parse(fromDate) : currentDate);
-
-    const toFilter = (currentDate, toDate) => (toDate
-        ? Date.parse(currentDate) < Date.parse(toDate) : currentDate);
-
-    // User.findById(userid).then((user) => {
-    //     const logs = user.log
-    //         .filter(l => fromFilter(l.date, from))
-    //         .filter(l => toFilter(l.date, to));
-    //     if (limit && limit < logs.length) {
-    //         res.send(logs.slice(0,limit));
-    //     } else {
-    //         res.send(logs);
-    //     }
-    // });
-
     User.findById(userId).then((user) => {
-        if (user == null){
-            res.send({"error":"User not found"});
-        }else{
+        if (user == null) {
+            res.send({"error": "User not found"});
+        } else {
             let results = user.log;
             let userId = user._id;
             let userName = user.username;
@@ -91,16 +69,14 @@ const getUserLog = (req, res) => {
             let fromDate = new Date(req.query.from);
             let toDate = new Date(req.query.to);
             let limit = Number(req.query.limit);
-            //check if to is defined
-            if (isValidDate(toDate)){
+
+            if (isValidDate(toDate)) {
                 log = log.filter((item) => (item.date >= fromDate && item.date <= toDate));
-                //check if just from defined
-            }else if(isValidDate(fromDate)){
-                log = log.filter((item)=>(item.date >= fromDate))
+            } else if (isValidDate(fromDate)) {
+                log = log.filter((item) => (item.date >= fromDate))
             }
-            //apply limit if defined and applicable
-            if (!isNaN(limit) && results.length > limit){
-                log = log.slice(0,limit);
+            if (!isNaN(limit) && results.length > limit) {
+                log = log.slice(0, limit);
             }
 
             res.json({
